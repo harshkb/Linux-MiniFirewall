fc9ffa8d4677c0968c2299e5fdda5ecc7845c034
harshkb <hkbansal1997@gmail.com>
2017-10-31 09:43:00 +0530

    Delete .mf_km.ko.cmd

--- module/.mf_km.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hkbansal/Desktop/miniFirewall/module/mf_km.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/hkbansal/Desktop/miniFirewall/module/mf_km.ko /home/hkbansal/Desktop/miniFirewall/module/mf_km.o /home/hkbansal/Desktop/miniFirewall/module/mf_km.mod.o
